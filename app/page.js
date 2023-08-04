dc76b80f2495584baa6689c4efa4025fbec359e9
httpArbaz <128707115+httpArbaz@users.noreply.github.com>
2023-08-04 21:55:06 +0500

    Initial commit from Create Next App

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="container">
+        <div className="">Map</div>
+        <div className="">Start</div>
+      </div>
+    </>
+  );
+}
